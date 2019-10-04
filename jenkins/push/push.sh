1356f28d81529c1f532a8efb4eab5d0f1114ff9c
vietawake <vietawake@gmail.com>
2019-10-04 22:59:43 +0700

    aaa

--- jenkins/push/push.sh
+++ jenkins/push/push.sh
@@ -5,10 +5,10 @@ echo "** Pushing image ***"
 echo "********************"
 
 IMAGE="laravelapp_php"
-
+TAG=latest
 echo "** Logging in ***"
 docker login -u vietawake -p $PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE vietawake/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$TAG vietawake/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
 docker push vietawake/$IMAGE:$BUILD_TAG
