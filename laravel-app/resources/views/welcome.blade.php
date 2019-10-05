a2320bc711732d4270b09d547cb8f1de1864509c
vietawake <vietawake@gmail.com>
2019-10-05 23:13:21 +0700

    comit

--- laravel-app/resources/views/welcome.blade.php
+++ laravel-app/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel OK 
+                    Laravel SumCoffee 
                 </div>
 
                 <div class="links">
